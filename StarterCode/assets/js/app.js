5b31af760b0947d468aa3baf58e8f0a93eee2a5b
clarkdavis6 <clarkdavis6@gmail.com>
2019-03-27 12:36:09 -0500

    js chart updates

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -3,9 +3,9 @@ var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-    top: 40,
+    top: 20,
     right: 40,
-    bottom: 75,
+    bottom: 60,
     left: 100
 };
 
@@ -13,10 +13,12 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
-    .attr("height", svgHeight);
+    .attr("height", svgHeight)
+    .append("g");
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -31,18 +33,18 @@ console.log(newsData);
 newsData.map(function (data) {
     data.poverty = +data.poverty;
     data.obesity = +data.obesity;
-    //data.smokers = +data.smokers;
-    //data.lacks
+    data.smokers = +data.smokers;
+    data.healthcare = +data.healthcare;
 });
 
 // Step 2: Create scale functions
 // ==============================
 var xLinearScale = d3.scaleLinear()
-    .domain([10, d3.max(newsData, d => d.poverty)])
+    .domain([8.5, d3.max(newsData, d => d.poverty)])
     .range([0, width]);
 
 var yLinearScale = d3.scaleLinear()
-    .domain([10, d3.max(newsData, d => d.obesity)])
+    .domain([20, d3.max(newsData, d => d.obesity)])
     .range([height, 0]);
 
 // Step 3: Create axis functions
@@ -52,26 +54,43 @@ var leftAxis = d3.axisLeft(yLinearScale);
 
 // Step 4: Append Axes to the chart
 // ==============================
-chartGroup.append("scatter")
+chartGroup.append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
 
-chartGroup.append("scatter")
+chartGroup.append("g")
     .call(leftAxis);
 
 // Step 5: Create Circles
 //http://bl.ocks.org/davegotz/bd54b56723c154d25eedde6504d30ad7
 // ==============================
-var circlesGroup = chartGroup.selectAll("circle")
-    .data(newsData)
-    .enter()
-    .append("circle")
+var circlesGroup = chartGroup.selectAll("circle").data(newsData).enter();
+
+    circlesGroup.append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.obesity))
     .attr("r", "15")
     .attr("fill", "pink")
     .attr("opacity", ".5")
-    .text(d => (d.abbr));//(still not appending the addr fo the state to the plot)
+    .on("click", function (data) {
+        toolTip.show(data, this);});
+    
+    circlesGroup.append("text")
+    .text(d => (d.abbr))
+    .attr("dx", d => xLinearScale(d.poverty))
+    .attr("dy", d => yLinearScale(d.obesity))
+    .attr("fontsize", "8")
+    .attr("class", "stateText")
+    // Step 8: Create event listeners to display and hide the tooltip
+// ==============================
+    .on("mouseover", function (data) {
+    toolTip.show(data, this);
+    })
+    // onmouseout event
+    .on("mouseout", function (data) {
+        toolTip.hide(data);
+    });
+
 
 // Step 6: Initialize tool tip
 // ==============================
@@ -88,13 +107,13 @@ chartGroup.call(toolTip);
 
 // Step 8: Create event listeners to display and hide the tooltip
 // ==============================
-circlesGroup.on("mouseover", function (data) {
-    toolTip.show(data, this);
-})
+//circlesGroup.on("mouseover", function (data) {
+    //toolTip.show(data, this);
+//})
     // onmouseout event
-    .on("mouseout", function (data) {
-        toolTip.hide(data);
-    });
+    //.on("mouseout", function (data) {
+        //toolTip.hide(data);
+    //});
 
 // Create axes labels
 chartGroup.append("text")
